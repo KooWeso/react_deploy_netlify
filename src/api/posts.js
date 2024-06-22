f7516037ad36a83df58fce82f1ea4a34640c4348
KooWeso <106434278+KooWeso@users.noreply.github.com>
2024-06-22 13:32:15 +0300

    comment > posts.js

--- src/api/posts.js
+++ src/api/posts.js
@@ -2,4 +2,6 @@ import axios from 'axios'
 
 export default axios.create({
   baseURL: 'http://localhost:3500',
+  //works only on your pc obviosly
+  //after deploying server with `npx json-server -p http://localhost:3500`
 })
